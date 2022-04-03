f864568df907d17dc8f040846552c8637f35d242
akapoorcern <akapoor@cern.ch>
2022-04-03 22:20:51 +0200

    First Commit

---
+++ getdata.sh
@@ -0,0 +1,6 @@
+python zdata.py -i "NSE:NIFTY 50" -f 2021-07-01 -t 2021-08-01 -n minute -o data1.csv
+python zdata.py -i "NSE:NIFTY 50" -f 2021-08-02 -t 2021-09-01 -n minute -o data2.csv
+python zdata.py -i "NSE:NIFTY 50" -f 2021-09-02 -t 2021-10-01 -n minute -o data3.csv
+python zdata.py -i "NSE:NIFTY 50" -f 2021-10-02 -t 2021-11-01 -n minute -o data4.csv
+python zdata.py -i "NSE:NIFTY 50" -f 2021-11-02 -t 2021-12-01 -n minute -o data5.csv
+python zdata.py -i "NSE:NIFTY 50" -f 2021-12-02 -t 2021-12-31 -n minute -o data6.csv
